25f23605c244c3dd8669230f6aac8acb0d86b9f5
LuiggiAndresZambranoFranco <74325889+LuiggiAndresZambranoFranco@users.noreply.github.com>
2022-10-31 13:31:34 -0500

    Delete index.php

--- practica/modelo/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once('consultas_mysql.php');
-    //$a = $_GET['id'];
-    $consultas= new ConsultasMysql();
-    $respuesta = $consultas->prueba('');
-    echo json_encode($respuesta);
-    
